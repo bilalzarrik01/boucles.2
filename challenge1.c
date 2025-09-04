8937a7075cc8781432a5e62121431a29d32478a2
bilalzarrik01 <bilalzarrik11@gmail.com>
2025-09-04 13:04:56 +0200

    ch1

---
+++ challenge1.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+int main(){
+
+int i , n ;
+
+
+printf("veuillez entrer votre nombre :");
+scanf("%d" , &n) ;
+
+for (i = 10; i >=1 ; i--)
+{ printf("%d * %d = %d \n" , i , n , i*n) ;
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
