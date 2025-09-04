aad01c6192d9d8bd02afb278a4bf9e86f31f3b58
bilalzarrik01 <bilalzarrik11@gmail.com>
2025-09-04 18:09:43 +0200

    ch5

---
+++ challeng5.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int main() {
+
+int n , i ,t =0 ;
+
+printf("veuillez entrer votre nombre svp :");
+scanf("%d" , &n);
+
+for ( i = 0; i <= n; i++)
+{
+    t += i ;  
+}
+
+printf("%d" , t);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  return 0 ;  
+}
