9d4afce958bcefab54184bd29c3360357c928212
bilalzarrik01 <bilalzarrik11@gmail.com>
2025-09-04 17:50:30 +0200

    ch4

--- challeng4.c
+++ challeng4.c
@@ -3,14 +3,18 @@
 
 int main() {
 
-int 
+int  a , b , i;
+
+printf("entrer le nombre : ");
+scanf("%d" , &a);
+while (a != 0)
+ {
+   b = a % 10 ;
+   i = i * 10 + b ;
+   a = a / 10 ;
+ }
+   printf("%d" , i);
 
 
-
-
-
-
-
-
-
+return 0 ;
 }
